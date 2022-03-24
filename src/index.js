64e53348f44ad1f21e55481bccadd4f9fb936c88
Guillermo Santos <guillermo.santosba@gmail.com>
2022-03-24 13:12:55 -0600

    Added scss to project.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';  // React Library
+import ReactDOM from 'react-dom';   // DOM library
+import './index.scss';  // Import CSS
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));   // Render the react app
